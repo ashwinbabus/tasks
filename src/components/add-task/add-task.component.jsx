6342f031d0b3946569ecc7be16a4985e2ccb6e29
ashwinbabus <ashwinbabus21@gmail.com>
2021-05-13 13:00:34 +0530

    font changed

--- src/components/add-task/add-task.component.jsx
+++ src/components/add-task/add-task.component.jsx
@@ -6,8 +6,8 @@ export default function AddTask({changeFormView , count}) {
     <div className="add_task__container">
 
       <div className="add_task__title">
-        <h4 className="add_task__title__title">TASKS</h4>
-        <h4 className="add_task__title__title">{count}</h4>
+        <h4>TASKS</h4>
+        <h4>{count}</h4>
       </div>
 
       <div className="add_task__action" onClick={() => changeFormView()}>
